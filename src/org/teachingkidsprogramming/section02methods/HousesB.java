c1845398c814ed9e9ad3cd4b7255e2c4def2b662
91084560 <31664292+91084560@users.noreply.github.com>
2017-09-18 10:49:48 -0700

    Varried my roofs

---
+++ src/org/teachingkidsprogramming/section02methods/HousesB.java
@@ -0,0 +1,54 @@
+package org.teachingkidsprogramming.section02methods;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
+
+public class HousesB
+{
+  public static void main(String[] args)
+  {
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    int height = 40;
+    drawHouse(height);
+    drawHouse(120);
+    drawHouse(90);
+    drawHouse(20);
+    drawHousePointyRoof(height);
+  }
+  private static void drawHousePointyRoof(int height)
+  {
+    Tortoise.setPenColor(Reds.Red);
+    Tortoise.move(height);
+    drawPointyRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouse(int height)
+  {
+    Tortoise.setPenColor(Reds.Red);
+    Tortoise.move(height);
+    drawFlatRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+  private static void drawPointyRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(45);
+  }
+}
