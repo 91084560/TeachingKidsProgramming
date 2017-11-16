0a009019ce35609df8bfc0763ddb8e9d39014ca7
91084560 <31664292+91084560@users.noreply.github.com>
2017-11-16 14:32:13 -0800

    finished spider web and DeepDive04(two of my own tests)

--- src/org/teachingkidsprogramming/section05recursion/SpiderWeb.java
+++ src/org/teachingkidsprogramming/section05recursion/SpiderWeb.java
@@ -1,36 +1,36 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#6
     Tortoise.setSpeed(10);
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
-    //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
-    //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
+    Tortoise.setPenWidth(1);
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Black);
     double currentLength = 10.5;
-    //    The current zoom is 1.1 --#8.2
     double currentZoom = 1.1;
-    //    Do the following 10 times --#10.1
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
+    for (int i = 0; i < 10; i++)
+    {
+      currentLength = weaveOneLayer(currentLength, currentZoom);
+      currentZoom = currentZoom * 1.3;
+    }
+  }
+  private static double weaveOneLayer(double currentLength, double currentZoom)
+  {
     for (int i = 0; i < 6; i++)
     {
       drawTriangle(currentLength);
       Tortoise.turn(360 / 6);
-      //            Increase the current length (of the line) by the current zoom --#8.1
       currentLength += currentZoom;
     }
-    //        ------------- End of weaveOneLayer recipe --#9.3
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    End Repeat --#10.2
+    return currentLength;
   }
   private static void drawTriangle(double currentLength)
   {
